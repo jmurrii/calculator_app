961e0b05651d138a34fc2118daf28a3c40ba229a
MAC <mac@MURRII-MacBook-Pro.local>
2022-10-25 10:43:31 +0100

    went back to refactor addition

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-const display = document.getElementById('display-text');
-const clearCalculator = document.getElementById('clear');
+const displayScreen = document.getElementById('displayScreen');
+const resetCalculator = document.getElementById('resetCalculator');
 const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
 const addition = document.getElementById('addition');
 const equals = document.getElementById('equals');
 
 let operand1 = 0;
 let tempTotal = 0;
-let total = 0;
+let theCurrentTotal = 0;
+let operatorValue;
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
+        console.log("the operator value is: ", operatorValue);
+        if (!operatorValue) {
+            theCurrentTotal = 0;
+        }
 
         const numberValue = number.childNodes[0].nodeValue;
+        // if(operatorValue == '-'){
+        //     numberValue = total - numberValue;
+        // }
         operand1 += numberValue;
-        console.log("Operand1 to be added", Number(operand1));
+        console.log("Operand1", Number(operand1));
         displayFunc(Number(operand1));
         return operand1;
 
@@ -21,38 +30,48 @@ numbers.forEach((number) => {
 });
 
 function displayFunc(displayNum) {
-    display.innerText = displayNum;
+    displayScreen.innerText = displayNum;
 }
 
+operators.forEach((operator) => {
 
-addition.addEventListener('click', () => {
-    additionFunc();
+    operator.addEventListener('click', () => {
+
+        operatorValue = operator.innerText;
+        if (operatorValue == '+') {
+            additionFunc();
+            return operatorValue = '+';
+        } else {
+            console.log("operator else block:");
+        }
+
+    })
+
 });
 
 function additionFunc() {
     tempTotal = Number(operand1);
     operand1 = 0;
-    total += tempTotal;
-    displayFunc(total);
-    console.log("addition evListener: ", total);
+    theCurrentTotal += tempTotal;
+    displayFunc(theCurrentTotal);
+    console.log("additionFunc theCurrentTotalIs: ", theCurrentTotal);
 }
 
 equals.addEventListener('click', () => {
-    accumulatorFunc();
+    console.log("equals operatorValue: ", operatorValue);
+    if (operatorValue == '+') {
+        additionFunc();
+    }
+    operatorValue = '';
 });
 
-function accumulatorFunc() {
-    tempTotal = Number(operand1);
-    operand1 = 0;
-    total += tempTotal;
-    displayFunc(total);
-    console.log("Equals: ", total);
-}
 
-clearCalculator.addEventListener('click', () => {
+
+resetCalculator.addEventListener('click', () => {
     operand1 = 0;
     tempTotal = 0;
-    total = 0;
+    theCurrentTotal = 0;
+    operatorValue = '';
 
-    display.innerText = 0;
+    displayScreen.innerText = 0;
 });
